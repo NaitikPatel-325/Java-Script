6dfd97e230f82e6f9d55b0368696cc6bf05dfbdb
NaitikPatel <np03022005@gmail.com>
2023-06-13 10:53:44 +0530

    new work

---
+++ Result/sc.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+//for prompt
+    var a = document.getElementById("slabel");
+    var b = document.getElementById("popup");
+    console.log("hii");
+
+    a.addEventListener("click", function () {
+        b.style.display = "block";
+    });
+   
+//promnpt marks :
+    var c = document.getElementById("m1");
+    var d = document.getElementById("m1");
+    var e = document.getElementById("m1");
+
+    
+
+});
